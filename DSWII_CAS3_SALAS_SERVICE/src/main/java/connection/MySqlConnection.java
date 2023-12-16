739aba8d4751572e88aef115e148f327306b1107
sadohu <sadohu26@yahoo.com>
2023-12-16 00:06:22 -0500

    Add MySqlConnection class for connecting to MySQL database

---
+++ DSWII_CAS3_SALAS_SERVICE/src/main/java/connection/MySqlConnection.java
@@ -0,0 +1,28 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection {
+
+	private static String url = "jdbc:mysql://localhost:3307/bdsalas";
+	private static String usuario = "root1";
+	private static String password = "123456";
+	private static Connection cn;
+
+	public static Connection conexion() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			cn = DriverManager.getConnection(url, usuario, password);
+			if (cn != null) {
+				// JOptionPane.showMessageDialog(null, "Conexion con base de datos Exitosa !!!","MENSAJE",JOptionPane.INFORMATION_MESSAGE);
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return cn;
+	}
+}
